c13c70f201330b36c5c18ec2934b8ce1ae46a2ce
Juan <juanrevengacsfm@gmail.com>
2026-07-13 20:47:19 -0400

    Archivo_conexión.php
    Juan Revenga Estudiante 2

---
+++ Archivo_conexión.php
@@ -0,0 +1,26 @@
+Archivo conexión.php: 
+
+<?php
+
+$host = "localhost";
+$dbname = "sistema";
+$user = "root";
+$pass = "";
+
+try {
+
+    $conexion = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $pass
+    );
+
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+
+    die("Error de conexión: ".$e->getMessage());
+
+}
+
+?>
